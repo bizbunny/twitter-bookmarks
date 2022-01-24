cab72b20542e5239c15445667a0341731624af2a
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-01-24 10:31:23 -0600

    new tweet

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -49,6 +49,22 @@ export default function GameMakingOrArtToolsPage(){
                             tweetLink="https://twitter.com/olguioo/status/1464655309251792908"
                         />
                     </Grid>
+                    <Grid item xl={3} lg={4} md={6} sm={12}>
+                        <Tweet
+                            backgroundImageURL={`url(https://pbs.twimg.com/profile_images/1267140053274374152/EUwmoaQl_400x400.jpg)`}
+                            srcImage="https://pbs.twimg.com/profile_images/1267140053274374152/EUwmoaQl_400x400.jpg"
+                            title="Nov 25, 2021"
+                            subheader="@olguioo"
+                            imageLink="https://pbs.twimg.com/media/FFD0p4bXMA0yvfq?format=jpg&name=large"
+                            imageAlt="Procreate"
+                            imageDescription="✏️ 20% Discount! Procreate brushes, palettes, tutorials, resources... 🍂🍄
+                            Code: olguioo20
+                            http://gumroad.com/olguioo
+                            
+                            "
+                            tweetLink="https://twitter.com/olguioo/status/1463938896614637579"
+                        />
+                    </Grid>
                 </Grid>
             </Container>
         </section>
