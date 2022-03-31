88fb9ffa6387bc010c598761f8c963321448ba8a
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2022-03-30 20:09:10 -0500

    minor updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => {
           <Container maxWidth="xl">
             <Grid container spacing={5}>
               <Grid item xl={12} lg={12} md={12} sm={12}>
-                <p>This website is to organize my twitter bookmarks. Things are still in work in progress.</p>
+                <p>This website is to organize my twitter bookmarks.</p>
           <     p>Click the picture below to visit my twitter.</p>
                 <Link to="https://twitter.com/anh_bizbunny">
                   <img src="https://pbs.twimg.com/profile_images/1343646540167061516/sGC-b4g-_400x400.jpg" alt="my twitter"/>
